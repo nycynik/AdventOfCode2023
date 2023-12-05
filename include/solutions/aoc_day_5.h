479675b95c07d85b33a0978211d04747df568792
Mike Lynch <nycynik@gmail.com>
2023-12-05 15:10:05 -0800

    day5 complete

--- include/solutions/aoc_day_5.h
+++ include/solutions/aoc_day_5.h
@@ -3,24 +3,23 @@
 
 #include "aoc_day.h"
 
-struct rangeMap
+struct RangeMap
 {
-    std::string type;
-    int src;
-    int dest;
-    int range;
+    long src;
+    long dest;
+    long range;
 };
 
 struct Alminac
 {
     vector<long> seeds;
-    vector<rangeMap> seed_soil;
-    vector<rangeMap> soil_fertalizer;
-    vector<rangeMap> fertalizer_water;
-    vector<rangeMap> water_light;
-    vector<rangeMap> light_temp;
-    vector<rangeMap> temp_humidity;
-    vector<rangeMap> humidity_location;
+    vector<RangeMap> seed_soil;
+    vector<RangeMap> soil_fertalizer;
+    vector<RangeMap> fertalizer_water;
+    vector<RangeMap> water_light;
+    vector<RangeMap> light_temp;
+    vector<RangeMap> temp_humidity;
+    vector<RangeMap> humidity_location;
 };
 
 class AocDay5 : public AocDay
@@ -29,6 +28,9 @@ private:
     Alminac read_input(string filename);
     template <typename T>
     vector<T> splitString(const std::string &input);
+    RangeMap read_range_map(const std::string &intut);
+    long findMapping(long id, vector<RangeMap> &r);
+    std::vector<std::pair<long, long>> convertToPairs(const std::vector<long> &input);
 
 public:
     AocDay5();
