7ddc6b13bf368f96b8356b28557f1441e50e058b
Mike Lynch <nycynik@gmail.com>
2023-12-06 20:32:03 -0800

    updated to include comments, still need to fix nextday

--- nextday.sh
+++ nextday.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# Git makes a branch for each day
 git co main
 git switch day${1} 2>/dev/null || git switch -c day${1};
 
+# creates the daily header file, and updates it
 cp ./include/solutions/aoc_day_0.h ./include/solutions/aoc_day_${1}.h
 sed -i -e "s/__AOC_DAY_0__/__AOC_DAY_${1}__/g" ./include/solutions/aoc_day_${1}.h
 sed -i -e "s/AocDay0/AocDay${1}/g" ./include/solutions/aoc_day_${1}.h
 
+# creates the daily source file, and updates it.
 cp ./src/solutions/aoc_day_0.cpp ./src/solutions/aoc_day_${1}.cpp
 sed -i -e "s/aoc_day_0.h/aoc_day_${1}.h/g" ./src/solutions/aoc_day_${1}.cpp
 sed -i -e "s/AocDay0/AocDay${1}/g" ./src/solutions/aoc_day_${1}.cpp
 sed -i -e "s/AocDay(0/AocDay(${1}/g" ./src/solutions/aoc_day_${1}.cpp
 
+# adds the day to aoc days
 sed -i -e "s/member to the map goes here/member to the map goes here\n    m_days[${1}] = new AocDay${1}();/" ./src/solutions/aoc_days.cpp
 sed -i -e "s/includes for each day will go here/includes for each day will go here\n#include \"aoc_day_${1}.h\"/" ./src/solutions/aoc_days.cpp
 
+# updates hte makefile 
 sed -i -e "s/# END daily solutions/build\/solutions\/aoc_day_${1}.o: src\/solutions\/aoc_day_${1}.cpp  \\\n	include\/solutions\/aoc_day_${1}.h \\\n	include\/solutions\/aoc_day.h \\\n	include\/common\/constants.h\n	g++ ${CPPFLAGS} -o build\/solutions\/aoc_day_1.o -c src\/solutions\/aoc_day_1.cpp${1}\n\n# END daily solutions/" ./Makefile
 sed -i -e "s/build\/solutions\/aoc_days.o build\/solutions\/aoc_day_0.o \/build\/solutions\/aoc_days.o build\/solutions\/aoc_day_0.o build\/solutions\/aoc_day_{$1}.o /" ./Makefile
 sed -i -e "s/	build/solutions/aoc_days.o  \\\	build/solutions/aoc_day_${1}.o  \\n/	build/solutions/aoc_days.o  \\/g" ./Makefile
 
-
+# finally get todays input
 curl "https://adventofcode.com/2023/day/${1}/input" \
   -o ./data/2023/day${1}_input.txt \
   -H 'authority: adventofcode.com' \
