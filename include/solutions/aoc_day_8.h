86812b23bd821ed7408526ba147bf8c8e0902038
Mike Lynch <nycynik@gmail.com>
2023-12-14 12:12:47 -0800

    day 8 working

--- include/solutions/aoc_day_8.h
+++ include/solutions/aoc_day_8.h
@@ -2,11 +2,18 @@
 #define __AOC_DAY_8__
 
 #include "aoc_day.h"
+#include <map>
+struct LR_data
+{
+    string lr;
+    map<string, pair<string, string>> data;
+};
 
 class AocDay8 : public AocDay
 {
     private:
-        vector<long> read_input(string filename);
+        LR_data read_input(string filename);
+
     public:
         AocDay8();
         ~AocDay8();
--- src/solutions/aoc_day_8.cpp
+++ src/solutions/aoc_day_8.cpp
@@ -17,57 +17,154 @@ AocDay8::~AocDay8()
 {
 }
 
-vector<long> AocDay8::read_input(string filename)
+LR_data AocDay8::read_input(string filename)
 {
     FileUtils fileutils;
     vector<string> raw_lines;
-    vector<long> data;
+    LR_data data;
     if (!fileutils.read_as_list_of_strings(filename, raw_lines))
     {
         cerr << "Error reading in the data from " << filename << endl;
         return data;
     }
+    int mode = 0;
     for (vector<string>::iterator iter = raw_lines.begin(); iter != raw_lines.end(); ++iter)
     {
-        long l;
-        string to_convert = *iter;
-        l = strtol(to_convert.c_str(), NULL, 10);
-        data.push_back(l);
+        string data_line = *iter;
+
+        if (data_line.empty())
+        {
+            mode++;
+        }
+        else
+        {
+            switch (mode)
+            {
+            case 0: // reading header line
+                data.lr = data_line;
+                break;
+            case 1: // all other lines
+                map<string, pair<string, string>> l;
+                string key = data_line.substr(0, 3);
+                string left = data_line.substr(data_line.find('(') + 1, 3);
+                string right = data_line.substr(data_line.find(", ") + 2, 3);
+                data.data[key] = pair<string, string>(left, right);
+                cout << key << ": -" << left << "- -" << right << "- " << data.data.size() << endl;
+                break;
+            }
+        }
     }
     return data;
 }
 
 string AocDay8::part1(string filename, vector<string> extra_args)
 {
-    vector<long> data = read_input(filename);
+    LR_data data = read_input(filename);
+
     long sum = 0;
-    for (vector<long>::iterator iter = data.begin(); iter != data.end(); ++iter)
+    string current_position = "AAA";
+    bool found = false;
+    long steps = 1;
+
+    while (!found)
     {
-        sum+=*iter;
+        for (int x = 0; x < data.lr.length(); x++)
+        {
+            pair<string, string> current_node = data.data[current_position];
+            cout << "Moving ";
+            if (data.lr[x] == 'L')
+            {
+                cout << "left ";
+                current_position = current_node.first;
+            }
+            else
+            {
+                cout << "right ";
+                current_position = current_node.second;
+            }
+            cout << "to node " << current_position << endl;
+            if (current_position == "ZZZ")
+            {
+                found = true;
+                break;
+            }
+            steps++;
+        }
     }
+
+    // for (LR_data.data::iterator iter = data.begin(); iter != data.end(); ++iter)
+    // {
+    //     sum+=*iter;
+    // }
     ostringstream out;
-    out << sum;
+    out << steps;
     return out.str();
 }
 
 string AocDay8::part2(string filename, vector<string> extra_args)
 {
-    if (extra_args.size() > 0)
+
+    LR_data data = read_input(filename);
+
+    long sum = 0;
+    // vector<string> current_positions = FindStartPositins(data.data);
+    cout << "Step 0: You are at ";
+    vector<string> current_positions;
+    for (auto const &[key, value] : data.data)
     {
-        cout << "There are " << extra_args.size() << " extra arguments given:" << endl;
-        for (vector<string>::iterator iter = extra_args.begin(); iter != extra_args.end(); ++iter)
+        if (key[2] == 'A')
         {
-            cout << "[" << *iter << "]" << endl;
+            current_positions.push_back(key);
+            cout << key << " ";
         }
     }
-    
-    vector<long> data = read_input(filename);
-    long sum = 0;
-    for (vector<long>::iterator iter = data.begin(); iter != data.end(); ++iter)
+    cout << "." << endl;
+
+    long found = 0;
+    long goal = current_positions.size();
+
+    long steps = 0;
+
+    while (found != goal)
     {
-        sum-=*iter;
+        for (int x = 0; x < data.lr.length(); x++)
+        {
+            steps++;
+            // cout << "Step " << steps << ": You choose all of the " << data.lr[x] << " paths, leading you to ";
+            found = 0;
+            for (int posIdx = 0; posIdx < current_positions.size(); posIdx++) // current_position : current_positions)
+            {
+                string current_position = current_positions[posIdx];
+
+                string next_position = "";
+                pair<string, string> current_node = data.data[current_position];
+                if (data.lr[x] == 'L')
+                {
+                    next_position = current_node.first;
+                }
+                else
+                {
+                    next_position = current_node.second;
+                }
+                // cout << next_position << " ";
+                if (next_position[2] == 'Z')
+                {
+                    // cout << "* ";
+                    found++;
+                }
+                current_positions[posIdx] = next_position;
+            }
+            // cout << endl;
+            if (found > 1)
+                cout << "Step " << steps << ": found " << found << endl;
+        }
     }
+
+    // for (LR_data.data::iterator iter = data.begin(); iter != data.end(); ++iter)
+    // {
+    //     sum+=*iter;
+    // }
     ostringstream out;
-    out << sum;
+    out << steps;
     return out.str();
 }
