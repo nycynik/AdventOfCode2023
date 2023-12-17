5813f3f1bb3e7efc6d418a075f90aca0c19e73e6
Mike Lynch <nycynik@gmail.com>
2023-12-16 21:03:57 -0800

    day11 mid way

--- include/solutions/aoc_day_11.h
+++ include/solutions/aoc_day_11.h
@@ -2,11 +2,21 @@
 #define __AOC_DAY_11__
 
 #include "aoc_day.h"
+#include "aoc_point.h"
 
+struct Universe
+{
+public:
+    long width;
+    long height;
+    vector<AOC_Point> galaxies;
+};
 class AocDay11 : public AocDay
 {
     private:
-        vector<long> read_input(string filename);
+        Universe read_input(string filename);
+        void fix_galaxies(Universe &universe);
+
     public:
         AocDay11();
         ~AocDay11();
